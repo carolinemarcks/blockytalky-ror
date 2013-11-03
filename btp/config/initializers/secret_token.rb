27b73e74e18ea497255188f5d0d92e148fdc8c18
Tucker Stone <tuckstone@gmail.com>
2013-11-03 17:25:33 -0500

    Adding skeleton for portal

---
+++ btp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Btp::Application.config.secret_token = '352fd44dc194c20abc41979ea8a75de7acd9e340ab3fd4d96f62e326d6bcbbc33c739873f8934efa972fa2ac3995aefa5ab15571b91ce60f7e38385989945b44'
