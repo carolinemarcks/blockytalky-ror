9cf949712dd3b59e487e8081587e60d05ec1c22a
Caroline Marcks <caroline.marcks@gmail.com>
2013-11-06 21:21:43 -0500

    added comments to simple recieve.rb

--- rabbit_demo/simple_helloworld/recieve.rb
+++ rabbit_demo/simple_helloworld/recieve.rb
@@ -3,16 +3,19 @@
 
 require "bunny"
 
-conn = Bunny.new
+conn = Bunny.new  
 conn.start
 
 ch = conn.create_channel
-q = ch.queue("hello")
+q = ch.queue("hello") #confirms the channel already exists, or creates it
 
 puts " [*] Waiting for messages in #{q.name}. To exit press CTRL+C"
-q.subscribe(:block => true) do |delivery_info,properties,body|
+
+q.subscribe(:block => true) do |delivery_info,properties,body| 
+    #this is the callback function for when messages are pushed (asynchronously from Rabbit)
+
     puts " [x] Recieved #{body}"
 
-    #cancel the consumer to exit
+    #cancel the consumer to exit -- if this line wasn't here it would hang continuously
     delivery_info.consumer.cancel
 end
