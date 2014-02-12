37ee3810d2230e6c1bc904366a7f135df5474c75
Eliot Alter <Eliot.Alter@gmail.com>
2014-02-11 19:30:46 -0500

    Don't allow people to change the user_id in update_attributes

--- btp/app/models/code.rb
+++ btp/app/models/code.rb
@@ -1,12 +1,12 @@
 class Code < ActiveRecord::Base
     # https://github.com/airblade/paper_trail
-    has_paper_trail :on => [:update]
+    has_paper_trail :on => [:update], :ignore => [:title]
 
     def owned_by?(user)
         self.user_id == (user.try(:id) || user)
     end
 
-    attr_accessible :codetext, :user_id, :title, :description
+    attr_accessible :codetext, :title, :description
     belongs_to :user
 
     # Test for error - renders page weirdly but data isn't lost
