1cdeb476fa14df72787a4994f80e249469843bdc
Nate Tarrh <nate@natetarrh.com>
2014-03-03 02:13:44 -0500

    Do not attempt to connect to the database during precompilation (heroku issue)

--- btp/config/application.rb
+++ btp/config/application.rb
@@ -38,6 +38,10 @@ module Btp
         # Enable the asset pipeline
         config.assets.enabled = true
 
+        # Do not attempt to connect to the database during precompilation
+        # see: https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting
+        config.assets.initialize_on_precompile = false
+
         # Version of your assets, change this if you want to expire all your assets
         config.assets.version = '1.0'
     end
